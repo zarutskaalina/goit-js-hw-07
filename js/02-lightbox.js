cb8d94f357db57f29b7eccb4ccd38b597284e817
Zarutska Alina <zaryckaalina@gmail.com>
2023-08-22 21:24:21 +0300

    deleted

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,26 +1,2 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-const galleryContainer = document.querySelector(".gallery");
-const itemMarkup = createGalleryMarkupItem(galleryItems);
-
-function createGalleryMarkupItem(item) {
-  return item
-    .map(
-      ({ preview, original, description }) =>
-        `<li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-       <img class="gallery__image" src="${preview}" alt="${description}" />
-    </a>
- </li>`
-    )
-    .join("");
-}
-
-galleryContainer.insertAdjacentHTML("beforeend", itemMarkup);
-
-const lightbox = new SimpleLightbox(".gallery__item a", {
-  captionsData: "alt",
-  //captionPosition: "bottom",
-  captionDelay: "250",
-});
